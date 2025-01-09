a0133c578552179ca0b79ca439891b4512a49f18
Sofiane Abou Abderrahim <sofianeabouabderrahim@gmail.com>
2025-01-09 21:27:52 +0100

    Working with Images & Changing Colors

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,4 +1,11 @@
-import { View, StyleSheet, TextInput, Button, Modal } from "react-native";
+import {
+  View,
+  StyleSheet,
+  TextInput,
+  Button,
+  Modal,
+  Image,
+} from "react-native";
 import React, { useState } from "react";
 
 const GoalInput = (props) => {
@@ -16,6 +23,10 @@ const GoalInput = (props) => {
   return (
     <Modal visible={props.visible} animationType="slide">
       <View style={styles.inputContainer}>
+        <Image
+          style={styles.image}
+          source={require("../assets/images/goal.png")}
+        />
         <TextInput
           style={styles.textInput}
           placeholder="Your course goal!"
@@ -42,10 +53,15 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    marginBottom: 24,
+
     padding: 16,
-    borderBottomWidth: 1,
-    borderBottomColor: "#cccccc",
+
+    backgroundColor: "#311b6b",
+  },
+  image: {
+    width: 100,
+    height: 100,
+    margin: 20,
   },
   textInput: {
     borderWidth: 1,
